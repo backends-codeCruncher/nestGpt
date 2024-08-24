02190cc607b39f548f220aec606c79c240a9d2b7
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-24 11:55:38 -0700

    Generación del audio

--- src/gpt/use-cases/text-to-audio.use-case.ts
+++ src/gpt/use-cases/text-to-audio.use-case.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import OpenAI from 'openai';
 import { Options } from '../interfaces/options.interface';
 
@@ -5,14 +7,30 @@ export const textToAudioUseCase = async (openai: OpenAI, options: Options) => {
   const { prompt, voice } = options;
 
   const voices = {
-    nova: 'nova',
     alloy: 'alloy',
+    echo: 'echo',
+    fable: 'fable',
+    onyx: 'onyx',
+    nova: 'nova',
+    shimmer: 'shimmer',
   };
 
   const selectedVoice = voices[voice] ?? 'nova';
 
-  return {
-    prompt,
-    selectedVoice,
-  };
+  const folderPath = path.resolve(__dirname, '../../../generated/audios/');
+  const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`);
+
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const audio = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: selectedVoice,
+    input: prompt,
+    response_format: 'mp3',
+  });
+
+  const buffer = Buffer.from(await audio.arrayBuffer());
+  fs.writeFileSync(speechFile, buffer);
+
+  return speechFile;
 };
