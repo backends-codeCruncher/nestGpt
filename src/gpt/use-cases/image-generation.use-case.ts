71b4229b8b879a51dc61ab689d20d239839d8e31
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-25 21:33:14 -0700

    Editar imágenes

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,25 +1,49 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import OpenAI from 'openai';
 import { Options } from '../interfaces/options.interface';
-import { downloadImageAsPng } from 'src/helpers';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
 
 export const imageGenerationUseCase = async (
   openai: OpenAI,
   options: Options,
 ) => {
   const { prompt, originalImage, maskImage } = options;
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt,
+      model: 'dall-e-3',
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
 
-  const response = await openai.images.generate({
-    prompt,
+    const url = await downloadImageAsPng(response.data[0].url);
+
+    return {
+      url,
+      openAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
     model: 'dall-e-3',
+    prompt: prompt,
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
-  const url = await downloadImageAsPng(response.data[0].url);
+  const localImagePath = await downloadImageAsPng(response.data[0].url);
+  const fileName = path.basename(localImagePath);
 
   return {
-    url,
+    url: '',
     openAIUrl: response.data[0].url,
     revised_prompt: response.data[0].revised_prompt,
   };
--- src/helpers/download-image-png.ts
+++ src/helpers/download-image-png.ts
@@ -21,3 +21,19 @@ export const downloadImageAsPng = async (url: string) => {
 
   return completePath;
 };
+
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imageNamePng = `${new Date().getTime()}-64.png`;
+
+  const completePath = path.join(folderPath, imageNamePng);
+
+  await sharp(imageBuffer).png().ensureAlpha().toFile(completePath);
+
+  return completePath;
+};
